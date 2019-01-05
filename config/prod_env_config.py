74a59169f1bc0a99fd7dec6e5c5efdcbeab621a8
Cosmin Poieana <cmin764@gmail.com>
2019-01-05 13:02:30 +0200

    Add testing configuration

---
+++ config/prod_env_config.py
@@ -0,0 +1,14 @@
+config = {
+	'ws': {
+		'uri':		'wss://ws.cex.io/ws/',
+	},
+	'rest': {
+		'uri':		'https://cex.io/api/',
+	},
+	'authorize': False,
+	'auth': {
+		'user_id':	'upXXXXXXXXX',
+		'key':		'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+		'secret':	'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+	},
+}
