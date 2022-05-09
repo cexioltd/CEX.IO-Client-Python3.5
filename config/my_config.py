e3f80073260348f108cf38b497ef4a94f9bc7335
846200 <100725908+846200@users.noreply.github.com>
2022-05-09 14:45:53 +0500

    Update my_config.py
    64306477488a10063ee2e47542cf8a81f0ec7e0c5351a724a46955567c20e496

--- config/my_config.py
+++ config/my_config.py
@@ -7,8 +7,8 @@ config = {
 	},
 	'authorize': False,
 	'auth': {
-		'user_id':	'upXXXXXXXXX',
-		'key':		'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-		'secret':	'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+		'user_id':	'up6310857',
+		'key':		'539e74cc657c2b7ad76aace327bbbfc938cea6c7e0c2504d96995c8806e0a0c7',
+		'secret':	'a81d6c777d2ad7a7e7477bea0879654fd002690bf46d597bd003d61f801ef781',
 	},
 }
