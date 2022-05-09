dd27d498ead32169329f250d3c24a2a5de6cc3fd
846200 <100725908+846200@users.noreply.github.com>
2022-05-09 16:38:42 +0500

    Update my_config.py

--- config/my_config.py
+++ config/my_config.py
@@ -5,10 +5,11 @@ config = {
 	'rest': {
 		'uri':		'https://cex.io/api/',
 	},
-	'authorize': False,
+	'authorize': True,
+	
 	'auth': {
-		'user_id':	'upXXXXXXXXX',
-		'key':		'xxxxxxxxxxxxxxxxxxxxxxxxxx',
-		'secret':	'xxxxxxxxxxxxxxxxxxxxxxxxxx',
+		'user_id':	'up150187825',
+		'key':		'539e74cc657c2b7ad76aace327bbbfc938cea6c7e0c2504d96995c8806e0a0c7',
+		'secret':	'JbDm5IVLxE6+ozWtpt5bTPLqBoIxZiyVe23eJd6veW921P64c6kbeuvSUb97QyOE43wjBxNUgqGni81Eb9u7Sw',
 	},
 }
